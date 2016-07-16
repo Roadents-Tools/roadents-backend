52df455096f6d254e98924bbf58d14d3294da794
ischeinkman <scheinkman.ilan@gmail.com>
2016-07-15 20:25:38 -0700

    LocationUtils can now convert between distance and walk time

--- src/main/java/org/tymit/projectdonut/utils/LocationUtils.java
+++ src/main/java/org/tymit/projectdonut/utils/LocationUtils.java
@@ -6,10 +6,13 @@ package org.tymit.projectdonut.utils;
 public class LocationUtils {
 
     /**
-     * Constansts for math
+     * Constants For Math
      **/
     private static final double EARTH_RADIUS_MI = 3958.75; //miles
     private static final double EARTH_RADIUS_KM = 6371.00; //kilometers
+    private static final double AVG_WALKING_SPEED_MPH = 3.1075;
+    private static final double AVG_WALKING_SPEED_KPH = 5.0;
+    private static final double SAFETY_FACTOR = 2;
 
 
     public static double distanceBetween(double[] l1, double[] l2, boolean miles) {
@@ -24,5 +27,16 @@ public class LocationUtils {
         double dist = c * ((miles) ? EARTH_RADIUS_MI : EARTH_RADIUS_KM);
 
         return dist;
+    }
+
+    public static long distanceToWalkTime(double distance, boolean miles) {
+        double hours = (miles) ? distance / AVG_WALKING_SPEED_MPH : distance / AVG_WALKING_SPEED_KPH;
+        double millis = hours * 1000.0 * 60.0 * 60.0 * SAFETY_FACTOR;
+        return (long) millis;
+    }
+
+    public static double timeToWalkDistance(long time, boolean miles) {
+        double timeHours = time / 1000.0 / 60.0 / 60.0;
+        return (miles) ? AVG_WALKING_SPEED_MPH * timeHours / SAFETY_FACTOR : AVG_WALKING_SPEED_KPH * timeHours / SAFETY_FACTOR;
     }
 }
