6b8950b5af622f58d5b23f8bc0ad354709cf1a03
ischeinkman <scheinkman.ilan@gmail.com>
2016-07-26 18:28:12 -0700

    Database now updates in the background on start

--- src/main/java/org/tymit/restcontroller/Application.java
+++ src/main/java/org/tymit/restcontroller/Application.java
@@ -3,6 +3,7 @@ package org.tymit.restcontroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.tymit.projectdonut.stations.updates.StationDbUpdater;
+import org.tymit.projectdonut.utils.LoggingUtils;
 
 /**
  * Created by ilan on 7/15/16.
@@ -15,6 +16,8 @@ public class Application {
     private static final long DB_UPDATE_INTERVAL = 1000l * 60l * 15l;
 
     public static void main(String[] args) {
+        LoggingUtils.setPrintImmediate(true);
+        StationDbUpdater.getUpdater().updateStationsAsync();
         StationDbUpdater.getUpdater().setBackgroundInterval(DB_UPDATE_INTERVAL);
         SpringApplication.run(Application.class, args);
     }
