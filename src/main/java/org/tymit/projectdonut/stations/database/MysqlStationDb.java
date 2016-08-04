a162f0ec3e4c487b3e49768ad204ddc103901c09
ischeinkman <scheinkman.ilan@gmail.com>
2016-08-03 23:58:45 -0700

    Added MysqlSupport class for MySQL logic; MysqlStationDB now inserts chains and stations on demand; MysqlStationDb now uses an internal connection pool

--- src/main/java/org/tymit/projectdonut/stations/database/MysqlStationDb.java
+++ src/main/java/org/tymit/projectdonut/stations/database/MysqlStationDb.java
@@ -1,21 +1,19 @@
 package org.tymit.projectdonut.stations.database;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import org.tymit.projectdonut.model.TimeModel;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.tymit.projectdonut.model.TransChain;
 import org.tymit.projectdonut.model.TransStation;
-import org.tymit.projectdonut.utils.LocationUtils;
 import org.tymit.projectdonut.utils.LoggingUtils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ilan on 7/9/16.
@@ -23,48 +21,63 @@ import java.util.stream.Collectors;
 public class MysqlStationDb implements StationDbInstance {
 
     public static final String[] DB_URLS = new String[]{"jdbc:mysql://192.168.1.71:3306/Donut"};
-    private static final double ERROR_MARGIN = 0.0001;
     private static final String USER = "donut";
     private static final String PASS = "donutpass";
 
-    private static final String STATION_TABLE_NAME = "transstation";
-    private static final String STATION_ID_KEY = STATION_TABLE_NAME + ".id";
-    private static final String STATION_NAME_KEY = STATION_TABLE_NAME + ".name";
-    private static final String STATION_LAT_KEY = STATION_TABLE_NAME + ".latitude";
-    private static final String STATION_LONG_KEY = STATION_TABLE_NAME + ".longitude";
-
-    private static final String CHAIN_TABLE_NAME = "transchain";
-    private static final String CHAIN_NAME_KEY = CHAIN_TABLE_NAME + ".name";
-    private static final String CHAIN_ID_KEY = CHAIN_TABLE_NAME + ".id";
-
-    private static final String STATION_CHAIN_COST_TABLE_NAME = "StationChainCosts";
-    private static final String COST_ID_KEY = STATION_CHAIN_COST_TABLE_NAME + ".id";
-    private static final String COST_STATION_KEY = STATION_CHAIN_COST_TABLE_NAME + ".stationId";
-    private static final String COST_CHAIN_KEY = STATION_CHAIN_COST_TABLE_NAME + ".chainId";
-    private static final String COST_SCHEDULE_KEY = STATION_CHAIN_COST_TABLE_NAME + ".scheduleJson";
-
-    private Connection connection;
+    private String url;
+    private boolean isUp;
+    private HikariDataSource connSource;
 
     public MysqlStationDb(String url) {
-        try {
-            connection = DriverManager.getConnection(url, USER, PASS);
-        } catch (SQLException e) {
-            LoggingUtils.logError(e);
-            connection = null;
-        }
+        this.url = url;
+        isUp = true;
+        connSource = new HikariDataSource(initSource(url));
     }
 
-    public void clearDb() throws SQLException {
-        for (String table : new String[]{STATION_TABLE_NAME, CHAIN_TABLE_NAME, STATION_CHAIN_COST_TABLE_NAME}) {
-            connection.createStatement().executeUpdate("DELETE FROM " + table);
+    private HikariConfig initSource(String url) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(USER);
+        config.setPassword(PASS);
+        config.setMaximumPoolSize(10);
+        return config;
+    }
+
+    public boolean removeItems(String whereClause, String[] tables) {
+        boolean success = true;
+
+        for (String table : tables) {
+            String deleteQuery = "DELETE FROM " + table + " WHERE " + whereClause;
+            try {
+                Connection connection = getConnection();
+                if (!connection.createStatement().execute(deleteQuery)) success = false;
+                connection.close();
+            } catch (SQLException e) {
+                success = false;
+            }
         }
+        return success;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return connSource.getConnection();
     }
 
     @Override
     public List<TransStation> queryStations(double[] center, double range, TransChain chain) {
+        Connection connection;
+        try {
+            connection = getConnection();
+        } catch (SQLException e) {
+            LoggingUtils.logError(e);
+            isUp = false;
+            return null;
+        }
+
         Map<TransStation, Integer> queryOut = null;
         try {
-            queryOut = getStationIdMap(center, range, chain);
+            queryOut = MysqlSupport.getStationIdMap(connection, center, range, chain);
+            connection.close();
         } catch (SQLException e) {
             LoggingUtils.logError(e);
         }
@@ -72,200 +85,62 @@ public class MysqlStationDb implements StationDbInstance {
         return new ArrayList<>(queryOut.keySet());
     }
 
-    private Map<TransStation, Integer> getStationIdMap(double[] center, double range, TransChain chain) throws SQLException {
-        String whereclause = "";
-        if (chain != null) {
-            whereclause += " WHERE " + CHAIN_NAME_KEY + "='" + chain.getName() + "'";
-        }
-
-        String query = "SELECT * FROM " + STATION_CHAIN_COST_TABLE_NAME
-                + " INNER JOIN " + STATION_TABLE_NAME + " ON " + COST_STATION_KEY + " = " + STATION_ID_KEY
-                + " INNER JOIN " + CHAIN_TABLE_NAME + " ON " + COST_CHAIN_KEY + " = " + CHAIN_ID_KEY
-                + whereclause;
-        ResultSet rawOut = connection.createStatement().executeQuery(query);
-        Map<String, TransChain> allChains = new HashMap<>();
-        Map<TransStation, Integer> outMap = new HashMap<>();
-        while (rawOut.next()) {
-            int id = rawOut.getInt(STATION_ID_KEY);
-            TransStation fromRow = getStationFromRow(rawOut, allChains);
-            if (fromRow == null) continue;
-            if (center == null) {
-                outMap.put(fromRow, id);
-                continue;
-            }
-            if (range < 0) continue;
-            double dist = LocationUtils.distanceBetween(center, fromRow.getCoordinates(), true);
-            if (dist <= range + ERROR_MARGIN) outMap.put(fromRow, id);
-        }
-
-        return outMap;
-    }
-
-    private TransStation getStationFromRow(ResultSet currentRow, Map<String, TransChain> availableChains) {
-        try {
-            String name = currentRow.getString(STATION_NAME_KEY);
-            double[] latlong = new double[]{currentRow.getDouble(STATION_LAT_KEY), currentRow.getDouble(STATION_LONG_KEY)};
-
-            String chainName = currentRow.getString(CHAIN_NAME_KEY);
-            TransChain chain;
-            if (availableChains.get(chainName) == null) {
-                chain = new TransChain(chainName);
-                availableChains.put(chainName, chain);
-            } else {
-                chain = availableChains.get(chainName);
-            }
-
-            String scheduleJson = currentRow.getString(COST_SCHEDULE_KEY);
-            List<TimeModel> schedule = decodeSchedule(scheduleJson);
-
-            TransStation rval = new TransStation(name, latlong, schedule, chain);
-            return rval;
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static List<TimeModel> decodeSchedule(String encodedSchedule) {
-        List<TimeModel> schedule = new ArrayList<>();
-        Gson gson = new Gson();
-        JsonParser parser = new JsonParser();
-
-        new ArrayList<>();
-        JsonArray array = parser.parse(encodedSchedule).getAsJsonArray();
-        for (JsonElement elm : array) {
-            TimeModel model = gson.fromJson(elm, TimeModel.class);
-            schedule.add(model);
-        }
-        return schedule;
-    }
-
     @Override
     public boolean putStations(List<TransStation> stations) {
-        try {
-            //Insert new chains
-            Map<String, Integer> allChains = getExistingChains();
-            Set<TransChain> newChains = stations.stream()
-                    .map(station -> station.getChain())
-                    .filter(chain -> !allChains.containsKey(chain.getName()))
-                    .collect(Collectors.toSet());
-            for (TransChain chain : newChains) {
-                int newId = insertNewChain(chain);
-                allChains.put(chain.getName(), newId);
-            }
 
-            //Insert new stations
-            Map<TransStation, Integer> allStations = getStationIdMap(null, 0, null);
-            Set<TransStation> toInsert = stations.stream()
-                    .filter(station -> !allStations.containsValue(station))
-                    .collect(Collectors.toSet());
-            for (TransStation newStation : toInsert) {
-                int newId = insertNewStation(newStation);
-                allStations.put(newStation, newId);
-            }
+        //Declare a cache that maps chain -> Id.
+        //We do this to a) only insert chains if we need to and
+        // b) so that we don't run out of memory.
+        LoadingCache<TransChain, Integer> chainToId = CacheBuilder.newBuilder()
+                .maximumSize(500)
+                .build(new CacheLoader<TransChain, Integer>() {
+                    @Override
+                    public Integer load(TransChain key) throws Exception {
+                        Connection con = getConnection();
+                        int id = MysqlSupport.insertOrGetChain(con, key);
+                        con.close();
+                        return id;
+                    }
+                });
+
+        //Ditto for the stations.
+        LoadingCache<TransStation, Integer> stationToId = CacheBuilder.newBuilder()
+                .maximumSize(500)
+                .build(new CacheLoader<TransStation, Integer>() {
+                           @Override
+                           public Integer load(TransStation key) throws Exception {
+                               Connection con = getConnection();
+                               int id = MysqlSupport.insertOrGetStation(con, key);
+                               con.close();
+                               return id;
+                           }
+                       }
+                );
 
             //Insert new schedules and costs
-            Map<Integer, Map<Integer, String>> existingCosts = getExistingSchedules();
-            for (TransStation station : stations) {
-                int stationId = allStations.get(station);
-                int chainId = allChains.get(station.getChain().getName());
-                String encodedSchedule = encodeSchedule(station.getSchedule());
-                if (existingCosts.get(chainId) == null || existingCosts.get(chainId).get(stationId) == null) {
-                    insertNewCosts(stationId, chainId, encodedSchedule);
-                } else if (!existingCosts.get(chainId).get(stationId).equals(encodedSchedule)) {
-                    updateCosts(stationId, chainId, encodedSchedule);
+        return stations.stream().map(station -> {
+            try {
+                int stationId = stationToId.get(station);
+                int chainId = chainToId.get(station.getChain());
+                String encodedSchedule = MysqlSupport.encodeSchedule(station.getSchedule());
+                Connection con = getConnection();
+                MysqlSupport.insertOrUpdateCosts(con, stationId, chainId, encodedSchedule);
+                con.close();
+                return true;
+            } catch (Exception e) {
+                LoggingUtils.logError(e);
+                return false;
                 }
-            }
-            return true;
-        } catch (Exception e) {
-            LoggingUtils.logError(e);
-            return false;
-        }
+        }).allMatch(Boolean::booleanValue);
     }
 
     @Override
     public boolean isUp() {
-        try {
-            return connection != null && !connection.isClosed() && !connection.isReadOnly();
-        } catch (SQLException e) {
-            LoggingUtils.logError(e);
-            return false;
-        }
+        return isUp;
     }
 
-    private Map<String, Integer> getExistingChains() throws SQLException {
-        Map<String, Integer> chainData = new HashMap<>();
-        String query = "SELECT * FROM " + CHAIN_TABLE_NAME;
-        ResultSet rawOut = connection.createStatement().executeQuery(query);
-        while (rawOut.next()) {
-            chainData.put(rawOut.getString(CHAIN_NAME_KEY), rawOut.getInt(CHAIN_ID_KEY));
-        }
-        return chainData;
-    }
-
-    private int insertNewChain(TransChain newChain) throws SQLException {
-        String insertQuery = String.format("INSERT INTO %s (%s) VALUES (%s)",
-                CHAIN_TABLE_NAME, CHAIN_NAME_KEY, "'" + newChain.getName() + "'"
-        );
-        connection.createStatement().executeUpdate(insertQuery);
-
-        String idQuery = String.format("SELECT %s FROM %s WHERE %s=%s",
-                CHAIN_ID_KEY, CHAIN_TABLE_NAME, CHAIN_NAME_KEY, "'" + newChain.getName() + "'"
-        );
-        ResultSet rs = connection.createStatement().executeQuery(idQuery);
-        while (rs.isBeforeFirst()) rs.next();
-        return rs.getInt(CHAIN_ID_KEY);
-    }
-
-    private int insertNewStation(TransStation station) throws SQLException {
-        String insertQuery = String.format("INSERT INTO %s (%s,%s,%s) VALUES (%s,%f,%f)",
-                STATION_TABLE_NAME, STATION_NAME_KEY, STATION_LAT_KEY, STATION_LONG_KEY,
-                "'" + station.getName() + "'", station.getCoordinates()[0], station.getCoordinates()[1]
-        );
-        connection.createStatement().executeUpdate(insertQuery);
-
-        String idQuery = String.format("SELECT %s FROM %s WHERE %s=%s AND %s=%f AND %s=%f",
-                STATION_ID_KEY, STATION_TABLE_NAME, STATION_NAME_KEY, "'" + station.getName() + "'",
-                STATION_LAT_KEY, station.getCoordinates()[0], STATION_LONG_KEY, station.getCoordinates()[1]
-        );
-        ResultSet rs = connection.createStatement().executeQuery(idQuery);
-        while (rs.isBeforeFirst()) rs.next();
-        return rs.getInt(STATION_ID_KEY);
-    }
-
-    private Map<Integer, Map<Integer, String>> getExistingSchedules() throws SQLException {
-        Map<Integer, Map<Integer, String>> rval = new HashMap<>();
-        String query = "SELECT * FROM " + STATION_CHAIN_COST_TABLE_NAME;
-        ResultSet rawOut = connection.createStatement().executeQuery(query);
-        while (rawOut.next()) {
-            int chainId = rawOut.getInt(COST_CHAIN_KEY);
-            int stationId = rawOut.getInt(COST_STATION_KEY);
-            String schedule = rawOut.getString(COST_SCHEDULE_KEY);
-            if (rval.get(chainId) == null) rval.put(chainId, new HashMap<>());
-            rval.get(chainId).put(stationId, schedule);
-        }
-        return rval;
-    }
-
-    private static String encodeSchedule(List<TimeModel> schedule) {
-        Gson gson = new Gson();
-        return gson.toJson(schedule);
-    }
-
-    private void insertNewCosts(int stationId, int chainId, String encodedSchedule) throws SQLException {
-        String insertQuery = String.format("INSERT INTO %s (%s,%s,%s) VALUES (%d,%d,%s)",
-                STATION_CHAIN_COST_TABLE_NAME, COST_STATION_KEY, COST_CHAIN_KEY, COST_SCHEDULE_KEY,
-                stationId, chainId, "'" + encodedSchedule + "'"
-        );
-
-        connection.createStatement().executeUpdate(insertQuery);
-    }
-
-    private void updateCosts(int stationId, int chainId, String encodedSchedule) throws SQLException {
-        String insertQuery = String.format("UPDATE %s SET %s = %s WHERE %s=%d AND %s=%d",
-                STATION_CHAIN_COST_TABLE_NAME, COST_SCHEDULE_KEY, "'" + encodedSchedule + "'",
-                COST_STATION_KEY, stationId, COST_CHAIN_KEY, chainId
-        );
-
-        connection.createStatement().executeUpdate(insertQuery);
+    @Override
+    public void close() {
+        connSource.close();
     }
 }
---
+++ src/main/java/org/tymit/projectdonut/stations/database/MysqlSupport.java
@@ -0,0 +1,214 @@
+package org.tymit.projectdonut.stations.database;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.tymit.projectdonut.model.TimeModel;
+import org.tymit.projectdonut.model.TransChain;
+import org.tymit.projectdonut.model.TransStation;
+import org.tymit.projectdonut.utils.LocationUtils;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by ilan on 7/31/16.
+ */
+public class MysqlSupport {
+    public static final String CHAIN_TABLE_NAME = "transchain";
+    public static final String CHAIN_ID_KEY = CHAIN_TABLE_NAME + ".id";
+    public static final String CHAIN_NAME_KEY = CHAIN_TABLE_NAME + ".name";
+    public static final String STATION_CHAIN_COST_TABLE_NAME = "StationChainCosts";
+    public static final String COST_SCHEDULE_KEY = STATION_CHAIN_COST_TABLE_NAME + ".scheduleJson";
+    public static final String COST_CHAIN_KEY = STATION_CHAIN_COST_TABLE_NAME + ".chainId";
+    public static final String COST_STATION_KEY = STATION_CHAIN_COST_TABLE_NAME + ".stationId";
+    public static final String COST_ID_KEY = STATION_CHAIN_COST_TABLE_NAME + ".id";
+    public static final double ERROR_MARGIN = 0.0001;
+    public static final String STATION_TABLE_NAME = "transstation";
+    public static final String STATION_LONG_KEY = STATION_TABLE_NAME + ".longitude";
+    public static final String STATION_LAT_KEY = STATION_TABLE_NAME + ".latitude";
+    public static final String STATION_NAME_KEY = STATION_TABLE_NAME + ".name";
+    public static final String STATION_ID_KEY = STATION_TABLE_NAME + ".id";
+
+    public static Map<TransStation, Integer> getStationIdMap(Connection connection, double[] center, double range, TransChain chain) throws SQLException {
+        String whereclause = "";
+        if (chain != null) {
+            whereclause += " WHERE " + CHAIN_NAME_KEY + "='" + chain.getName().replaceAll("'", "\\'") + "'";
+        }
+
+        String query = "SELECT * FROM " + STATION_CHAIN_COST_TABLE_NAME
+                + " INNER JOIN " + STATION_TABLE_NAME + " ON " + COST_STATION_KEY + " = " + STATION_ID_KEY
+                + " INNER JOIN " + CHAIN_TABLE_NAME + " ON " + COST_CHAIN_KEY + " = " + CHAIN_ID_KEY
+                + whereclause;
+
+        Statement stm = connection.createStatement();
+        ResultSet rawOut = stm.executeQuery(query);
+
+        Map<String, TransChain> allChains = new HashMap<>();
+        Map<TransStation, Integer> outMap = new HashMap<>();
+
+        while (rawOut.next()) {
+            int id = rawOut.getInt(STATION_ID_KEY);
+            TransStation fromRow = getStationFromRow(rawOut, allChains);
+            if (fromRow == null) continue;
+            if (center == null) {
+                outMap.put(fromRow, id);
+                continue;
+            }
+            if (range < 0) continue;
+            double dist = LocationUtils.distanceBetween(center, fromRow.getCoordinates(), true);
+            if (dist <= range + ERROR_MARGIN) outMap.put(fromRow, id);
+        }
+
+
+        stm.close();
+        rawOut.close();
+        return outMap;
+    }
+
+    public static TransStation getStationFromRow(ResultSet currentRow, Map<String, TransChain> availableChains) throws SQLException {
+
+        String name = currentRow.getString(STATION_NAME_KEY);
+        double[] latlong = new double[] { currentRow.getDouble(STATION_LAT_KEY), currentRow.getDouble(STATION_LONG_KEY) };
+
+        String chainName = currentRow.getString(CHAIN_NAME_KEY);
+        TransChain chain;
+        if (availableChains.get(chainName) == null) {
+            chain = new TransChain(chainName);
+            availableChains.put(chainName, chain);
+        } else {
+            chain = availableChains.get(chainName);
+        }
+
+        String scheduleJson = currentRow.getString(COST_SCHEDULE_KEY);
+        List<TimeModel> schedule = decodeSchedule(scheduleJson);
+
+        TransStation rval = new TransStation(name, latlong, schedule, chain);
+        return rval;
+
+    }
+
+    public static List<TimeModel> decodeSchedule(String encodedSchedule) {
+        List<TimeModel> schedule = new ArrayList<>();
+        Gson gson = new Gson();
+        JsonParser parser = new JsonParser();
+
+        JsonArray array = parser.parse(encodedSchedule).getAsJsonArray();
+        for (JsonElement elm : array) {
+            TimeModel model = gson.fromJson(elm, TimeModel.class);
+            schedule.add(model);
+        }
+        return schedule;
+    }
+
+    public static String encodeSchedule(List<TimeModel> schedule) {
+        Gson gson = new Gson();
+        return gson.toJson(schedule);
+    }
+
+    public static int insertOrGetChain(Connection connection, TransChain newChain) throws SQLException {
+        String idQuery = String.format("SELECT %s FROM %s WHERE %s=%s",
+                CHAIN_ID_KEY, CHAIN_TABLE_NAME, CHAIN_NAME_KEY, "'" + newChain.getName().replaceAll("'", "\\'") + "'"
+        );
+
+        Statement stm = connection.createStatement();
+        ResultSet rs = stm.executeQuery(idQuery);
+        if (rs.next()) {
+            int id = rs.getInt(CHAIN_ID_KEY);
+            return id;
+        }
+        rs.close();
+        stm.close();
+
+        String insertQuery = String.format("INSERT INTO %s (%s) VALUES (%s)",
+                CHAIN_TABLE_NAME, CHAIN_NAME_KEY, "'" + newChain.getName().replaceAll("'", "\\'") + "'"
+        );
+        stm = connection.createStatement();
+        stm.executeUpdate(insertQuery);
+        stm.close();
+
+        stm = connection.createStatement();
+        rs = stm.executeQuery(idQuery);
+        while (rs.isBeforeFirst()) rs.next();
+        int id = rs.getInt(CHAIN_ID_KEY);
+        rs.close();
+        stm.close();
+        return id;
+    }
+
+    public static int insertOrGetStation(Connection connection, TransStation station) throws SQLException {
+        String idQuery = String.format("SELECT %s FROM %s WHERE %s=%s AND %s=%f AND %s=%f",
+                STATION_ID_KEY, STATION_TABLE_NAME, STATION_NAME_KEY, "'" + station.getName() + "'",
+                STATION_LAT_KEY, station.getCoordinates()[0], STATION_LONG_KEY, station.getCoordinates()[1]
+        );
+        Statement stm = connection.createStatement();
+        ResultSet rs = stm.executeQuery(idQuery);
+        if (rs.next()) {
+            int id = rs.getInt(STATION_ID_KEY);
+            rs.close();
+            stm.close();
+            return id;
+        }
+
+        String insertQuery = String.format("INSERT INTO %s (%s,%s,%s) VALUES (%s,%f,%f)",
+                STATION_TABLE_NAME, STATION_NAME_KEY, STATION_LAT_KEY, STATION_LONG_KEY,
+                "'" + station.getName().replaceAll("'", "\\'") + "'", station.getCoordinates()[0], station.getCoordinates()[1]
+        );
+
+        stm = connection.createStatement();
+        stm.executeUpdate(insertQuery);
+        stm.close();
+
+        stm = connection.createStatement();
+        rs = stm.executeQuery(idQuery);
+        while (rs.isBeforeFirst()) rs.next();
+        int id = rs.getInt(STATION_ID_KEY);
+        rs.close();
+        stm.close();
+        return id;
+    }
+
+    public static void insertOrUpdateCosts(Connection connection, int stationId, int chainId, String encodedSchedule) throws SQLException {
+        String existingQuery = String.format("SELECT * FROM %s WHERE %s=%d AND %s=%d",
+                STATION_CHAIN_COST_TABLE_NAME, COST_STATION_KEY, stationId, COST_CHAIN_KEY, chainId);
+
+        Statement stm = connection.createStatement();
+        ResultSet rs = stm.executeQuery(existingQuery);
+        if (rs.next()) {
+            updateCosts(connection, stationId, chainId, encodedSchedule);
+        } else {
+            insertNewCosts(connection, stationId, chainId, encodedSchedule);
+        }
+        stm.close();
+
+    }
+
+    public static void insertNewCosts(Connection connection, int stationId, int chainId, String encodedSchedule) throws SQLException {
+        String insertQuery = String.format("INSERT INTO %s (%s,%s,%s) VALUES (%d,%d,%s)",
+                STATION_CHAIN_COST_TABLE_NAME, COST_STATION_KEY, COST_CHAIN_KEY, COST_SCHEDULE_KEY,
+                stationId, chainId, "'" + encodedSchedule.replaceAll("'", "\\'") + "'"
+        );
+
+        Statement stm = connection.createStatement();
+        stm.executeUpdate(insertQuery);
+        stm.close();
+    }
+
+    public static void updateCosts(Connection connection, int stationId, int chainId, String encodedSchedule) throws SQLException {
+        String insertQuery = String.format("UPDATE %s SET %s = %s WHERE %s=%d AND %s=%d",
+                STATION_CHAIN_COST_TABLE_NAME, COST_SCHEDULE_KEY, "'" + encodedSchedule.replaceAll("'", "\\'") + "'",
+                COST_STATION_KEY, stationId, COST_CHAIN_KEY, chainId
+        );
+
+        Statement stm = connection.createStatement();
+        stm.executeUpdate(insertQuery);
+        stm.close();
+    }
+}
