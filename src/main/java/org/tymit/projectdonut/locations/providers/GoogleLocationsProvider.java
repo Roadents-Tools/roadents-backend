1e9bcb04a8622bc03ec5903a7c735754c66eb50d
ischeinkman <scheinkman.ilan@gmail.com>
2016-08-26 01:35:13 -0700

    Rewrote the Google provider to use retrofit instead of an external library.

--- src/main/java/org/tymit/projectdonut/locations/providers/GoogleLocationsProvider.java
+++ src/main/java/org/tymit/projectdonut/locations/providers/GoogleLocationsProvider.java
@@ -1,88 +1,122 @@
 package org.tymit.projectdonut.locations.providers;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import org.slf4j.LoggerFactory;
+import okhttp3.ResponseBody;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.tymit.projectdonut.model.DestinationLocation;
 import org.tymit.projectdonut.model.LocationType;
 import org.tymit.projectdonut.utils.LocationUtils;
 import org.tymit.projectdonut.utils.LoggingUtils;
-import se.walkercrou.places.GooglePlaces;
-import se.walkercrou.places.Param;
-import se.walkercrou.places.Place;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
- * Created by ilan on 7/7/16.
+ * Created by ilan on 8/25/16.
  */
 public class GoogleLocationsProvider implements LocationProvider {
 
     public static final String[] API_KEYS = {
             "AIzaSyB0pBdXuC4VRte73qnVtE5pLmxNs3ju0Gg"
     };
-    private static final int MAX_QUERY_CALLS = 1;
+
+    private static final String BASE_URL = "https://maps.googleapis.com/";
     private static double MILES_TO_METERS = 1609.344;
 
-    static {
-        disableApacheLogging();
-    }
 
-    private GooglePlaces gmaps;
-    private int queryCalls;
+    String key;
+    boolean isWorking;
+    RestInterface rest;
 
     public GoogleLocationsProvider() {
-        gmaps = new GooglePlaces(API_KEYS[0]);
-        queryCalls = 0;
-    }
-
-    private static void disableApacheLogging() {
-        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-        root.setLevel(Level.OFF);
+        key = API_KEYS[0];
+        isWorking = true;
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .build();
+        rest = retrofit.create(RestInterface.class);
     }
 
     @Override
     public boolean isUsable() {
-        return true; //We only have the one, so we always have to use it no matter what.
+        return isWorking;
     }
 
     @Override
     public boolean isValidType(LocationType type) {
-        return true; //Google can basically handle anything.
+        return true;
     }
 
     @Override
     public List<DestinationLocation> queryLocations(double[] center, double range, LocationType type) {
-        if (queryCalls >= MAX_QUERY_CALLS) {
-            queryCalls = 0;
-            gmaps = new GooglePlaces(API_KEYS[0]);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                LoggingUtils.logError(e);
-            }
-        }
-        queryCalls++;
+        Call<ResponseBody> result = rest.getLocations(center[0] + "," + center[1], type.getEncodedname(), "distance", key);
+
+        Response<ResponseBody> response;
         try {
-            Set<Place> places = gmaps.getNearbyPlacesRankedByDistance(center[0], center[1],
-                    Param.name("type").value(type.getEncodedname())).stream()
-                    .filter(place -> center == null
-                            || range < 0
-                            || LocationUtils.distanceBetween(center, new double[]{place.getLatitude(), place.getLongitude()}, true) < range)
-                    .collect(Collectors.toSet());
-            List<DestinationLocation> rval = new ArrayList<>();
-            for (Place place : places) {
-                String placeName = place.getName();
-                rval.add(new DestinationLocation(placeName, type, new double[]{place.getLatitude(), place.getLongitude()}));
-            }
-            return rval;
-        } catch (Exception e) {
+            response = result.execute();
+        } catch (IOException e) {
             LoggingUtils.logError(e);
-            return new ArrayList<>();
+            isWorking = false;
+            return Collections.EMPTY_LIST;
         }
+
+        if (!response.isSuccessful()) {
+            LoggingUtils.logError("GoogleRetrofitProvider", "Response failed.\nResponse: " + response.raw().toString());
+            isWorking = false;
+        }
+
+
+        try {
+            String raw = new String(response.body().bytes());
+            JSONObject obj = new JSONObject(raw);
+            JSONArray arr = obj.getJSONArray("results");
+            return getLocationsFromGglJson(arr, type).stream()
+                    .filter(location -> LocationUtils.distanceBetween(center, location.getCoordinates(), true) < range)
+                    .collect(Collectors.toList());
+        } catch (JSONException e) {
+            LoggingUtils.logError(e);
+            LoggingUtils.logError("GoogleRetrofitProvider", "JSON: " + response.toString());
+            return Collections.EMPTY_LIST;
+        } catch (IOException e) {
+            LoggingUtils.logError(e);
+            LoggingUtils.logError("GoogleRetrofitProvider", "JSON: " + response.toString());
+            return Collections.EMPTY_LIST;
+        }
+    }
+
+    private static List<DestinationLocation> getLocationsFromGglJson(JSONArray arr, LocationType type) {
+        int size = arr.length();
+
+        return IntStream.range(0, size)
+                .boxed().parallel()
+                .map(arr::getJSONObject)
+                .map(currObj -> {
+
+                    JSONObject coords = currObj.getJSONObject("geometry").getJSONObject("location");
+                    double[] latlong = new double[] { coords.getDouble("lat"), coords.getDouble("lng") };
+
+                    String name = currObj.getString("name");
+                    return new DestinationLocation(name, type, latlong);
+                })
+                .collect(Collectors.toList());
+
+    }
+
+    private static interface RestInterface {
+
+        @GET("maps/api/place/nearbysearch/json")
+        Call<ResponseBody> getLocations(@Query("location") String latLong, @Query("keyword") String keyword,
+                                        @Query("rankby") String rankBy, @Query("key") String apiKey);
+
     }
 
 
