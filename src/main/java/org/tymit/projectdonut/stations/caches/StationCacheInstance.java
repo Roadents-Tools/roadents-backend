f09f28b971e46ed6356b8f166b9c3f65c16859ae
ischeinkman <scheinkman.ilan@gmail.com>
2017-04-14 19:25:52 -0700

    Reworked caching interfaces

--- src/main/java/org/tymit/projectdonut/stations/caches/StationCacheInstance.java
+++ src/main/java/org/tymit/projectdonut/stations/caches/StationCacheInstance.java
@@ -1,5 +1,8 @@
 package org.tymit.projectdonut.stations.caches;
 
+import org.tymit.projectdonut.model.TimeDelta;
+import org.tymit.projectdonut.model.TimePoint;
+import org.tymit.projectdonut.model.TransChain;
 import org.tymit.projectdonut.model.TransStation;
 
 import java.util.List;
@@ -8,12 +11,8 @@ import java.util.List;
  * Created by ilan on 8/31/16.
  */
 public interface StationCacheInstance {
-    void cacheStations(double[] center, double range, List<TransStation> stations);
+    boolean cacheStations(double[] center, double range, TimePoint startTime, TimeDelta maxDelta, List<TransStation> stations);
 
 
-    List<TransStation> getCachedStations(double[] center, double range);
-
-    int getSize();
-
-    void clear();
+    List<TransStation> getCachedStations(double[] center, double range, TimePoint start, TimeDelta maxDelta, TransChain chain);
 }
--- src/main/java/org/tymit/projectdonut/stations/caches/StationChainCacheHelper.java
+++ src/main/java/org/tymit/projectdonut/stations/caches/StationChainCacheHelper.java
@@ -1,10 +1,12 @@
 package org.tymit.projectdonut.stations.caches;
 
+import org.tymit.projectdonut.model.TimeDelta;
+import org.tymit.projectdonut.model.TimePoint;
 import org.tymit.projectdonut.model.TransChain;
 import org.tymit.projectdonut.model.TransStation;
-import org.tymit.projectdonut.utils.LocationUtils;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -15,7 +17,6 @@ import java.util.stream.Collectors;
 public class StationChainCacheHelper {
 
     private static final StationCacheInstance[] allStationInstances = initializeStationInstanceList();
-    private static final ChainCacheInstance[] allChainInstances = initializeChainInstanceList();
     private static final StationChainCacheHelper instance = new StationChainCacheHelper();
 
     private StationChainCacheHelper() {
@@ -23,60 +24,29 @@ public class StationChainCacheHelper {
     }
 
     private static StationCacheInstance[] initializeStationInstanceList() {
-        return new StationCacheInstance[] { new MemoryMapStationCache() };
-    }
-
-    private static ChainCacheInstance[] initializeChainInstanceList() {
-        return new ChainCacheInstance[] { new MemoryMapChainCache() };
+        return (StationCacheInstance[]) Arrays.stream(PostgresqlExternalCache.DB_URLS)
+                .map(PostgresqlExternalCache::new)
+                .toArray();
     }
 
     public static StationChainCacheHelper getHelper() {
         return instance;
     }
 
-    public List<TransStation> getCachedStations(double[] center, double range, TransChain chain) {
-        if (center == null && chain == null) return null;
-
-        if (chain != null) {
-            List<TransStation> cached = getCachedStations(chain);
-            return (cached == null) ? null : cached.stream()
-                    .filter(station -> center == null || LocationUtils.distanceBetween(center, station.getCoordinates(), true) < range)
-                    .collect(Collectors.toList());
-        }
-
-        return getCachedStations(center, range);
-    }
-
-    private List<TransStation> getCachedStations(double[] center, double range) {
-        if (center == null || range <= 0) return null;
+    public List<TransStation> getCachedStations(double[] center, double range, TimePoint startTime, TimeDelta maxDelta, TransChain chain) {
         return Arrays.stream(allStationInstances)
-                .map(instance -> instance.getCachedStations(center, range))
+                .parallel()
+                .map(cache -> cache.getCachedStations(center, range, startTime, maxDelta, chain))
                 .filter(Objects::nonNull)
-                .findFirst()
-                .orElse(null);
+                .flatMap(Collection::parallelStream)
+                .collect(Collectors.toList());
     }
 
-    private List<TransStation> getCachedStations(TransChain chain) {
-        if (chain == null) return null;
-        return Arrays.stream(allChainInstances)
-                .map(instance -> instance.getCachedStations(chain))
-                .filter(Objects::nonNull)
-                .findFirst()
-                .orElse(null);
-    }
+    public boolean cacheStations(double[] center, double range, TimePoint startTime, TimeDelta maxDelta, List<TransStation> stations) {
+        return Arrays.stream(allStationInstances)
+                .parallel()
+                .map(cache -> cache.cacheStations(center, range, startTime, maxDelta, stations))
+                .anyMatch(Boolean::booleanValue);
 
-    public void cacheStations(double[] center, double range, TransChain chain, List<TransStation> stations) {
-        if (stations == null || stations.isEmpty()) return;
-        if (center != null && chain != null) return;
-        if (chain != null) cacheStations(chain, stations);
-        cacheStations(center, range, stations);
-    }
-
-    private void cacheStations(double[] center, double range, List<TransStation> stations) {
-        allStationInstances[0].cacheStations(center, range, stations);
-    }
-
-    private void cacheStations(TransChain chain, List<TransStation> stations) {
-        allChainInstances[0].cacheChain(chain, stations);
     }
 }
