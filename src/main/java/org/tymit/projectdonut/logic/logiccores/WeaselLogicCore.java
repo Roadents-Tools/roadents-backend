69c8bf73e577f49d1b9a4fd562071df47ab2a6aa
ischeinkman <scheinkman.ilan@gmail.com>
2017-05-02 21:46:23 -0700

    Added empty WeaselLogicCore class

---
+++ src/main/java/org/tymit/projectdonut/logic/logiccores/WeaselLogicCore.java
@@ -0,0 +1,23 @@
+package org.tymit.projectdonut.logic.logiccores;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by ilan on 4/30/17.
+ */
+public class WeaselLogicCore implements LogicCore {
+
+    public static final String TAG = "WEASEL";
+
+    @Override
+    public Map<String, List<Object>> performLogic(Map<String, Object> args) {
+        return null;
+    }
+
+    @Override
+    public String getTag() {
+        return TAG;
+    }
+
+}
