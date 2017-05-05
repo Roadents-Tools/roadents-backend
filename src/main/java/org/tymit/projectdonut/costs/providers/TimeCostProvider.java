f5819e326de7d12e9e1d7d9ce0eeb3059bec5515
ischeinkman <scheinkman.ilan@gmail.com>
2017-05-05 00:39:03 -0700

    Made TimeCostProvider abstract to allow different backing services

--- src/main/java/org/tymit/projectdonut/costs/providers/TimeCostProvider.java
+++ src/main/java/org/tymit/projectdonut/costs/providers/TimeCostProvider.java
@@ -1,15 +1,26 @@
 package org.tymit.projectdonut.costs.providers;
 
+import com.google.common.collect.Lists;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONObject;
 import org.tymit.projectdonut.costs.CostArgs;
 import org.tymit.projectdonut.model.LocationPoint;
 import org.tymit.projectdonut.model.TimeDelta;
+import org.tymit.projectdonut.model.TimePoint;
+import org.tymit.projectdonut.utils.LoggingUtils;
 
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
+import java.util.StringJoiner;
 import java.util.function.Predicate;
 
 /**
  * Created by ilan on 5/2/17.
  */
-public class TimeCostProvider implements CostProvider {
+public abstract class TimeCostProvider implements CostProvider {
 
     /*
     PARAM:
@@ -27,6 +38,53 @@ public class TimeCostProvider implements CostProvider {
     public static final String COMPARISON_TAG = "comparison";
     public static final String COMPARE_VALUE_TAG = "compareto";
     public static final String POINT_TWO_TAG = "p2";
+
+    private OkHttpClient client = new OkHttpClient();
+    private String apiKey;
+    private int calls = 0;
+
+
+    public TimeCostProvider(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public String getTag() {
+        return TAG;
+    }
+
+    @Override
+    public boolean isWithinCosts(CostArgs arg) {
+        return extractComparison(arg).test((TimeDelta) getCostValue(arg));
+    }
+
+    @Override
+    public Object getCostValue(CostArgs arg) {
+        double[] start = extractStart(arg);
+        double[] end = extractEnd(arg);
+        long startTime = extractStartTime(arg);
+
+        String url = buildCallUrl(Lists.newArrayList(start), Lists.newArrayList(end), startTime);
+        JSONObject obj = runCall(url);
+        if (obj == null) return null;
+        TimeDelta[][] timeMatrix = extractTimeMatrix(obj);
+        if (timeMatrix == null
+                || timeMatrix.length < 1
+                || timeMatrix[0].length < 1
+                || timeMatrix[0][0].getDeltaLong() < 0) {
+            return null;
+        }
+        return timeMatrix[0][0];
+    }
+
+    protected abstract TimeDelta[][] extractTimeMatrix(JSONObject json);
+
+    @Override
+    public boolean isUp() {
+        return calls <= getMaxCalls();
+    }
+
+    protected abstract int getMaxCalls();
 
     /**
      * Argument extraction methods
@@ -69,24 +127,48 @@ public class TimeCostProvider implements CostProvider {
         return rval;
     }
 
-    @Override
-    public String getTag() {
-        return TAG;
+    private static long extractStartTime(CostArgs args) {
+        Object startTimeArg = args.getArgs().get(START_TIME_TAG);
+        if (startTimeArg instanceof Long) return (Long) startTimeArg;
+        if (startTimeArg instanceof Integer) return (Integer) startTimeArg;
+        if (startTimeArg instanceof TimePoint) return ((TimePoint) startTimeArg).getUnixTime();
+        if (startTimeArg instanceof Calendar) return ((Calendar) startTimeArg).getTimeInMillis();
+        return -1;
     }
 
-    @Override
-    public boolean isWithinCosts(CostArgs arg) {
-        return extractComparison(arg).test((TimeDelta) getCostValue(arg));
+    /**
+     * Call running methods
+     **/
+    private String buildCallUrl(List<double[]> srcs, List<double[]> dests, long startTime) {
+        String srcStr = srcs.parallelStream()
+                .map(latlng -> String.format(getLocationFormat(), latlng[0], latlng[1]))
+                .collect(() -> new StringJoiner(getLocationSeparator()), StringJoiner::add, StringJoiner::merge)
+                .toString();
+
+        String destStr = dests.parallelStream()
+                .map(latlng -> String.format(getLocationFormat(), latlng[0], latlng[1]))
+                .collect(() -> new StringJoiner(getLocationSeparator()), StringJoiner::add, StringJoiner::merge)
+                .toString();
+
+        return String.format(getCallUrlFormat(), srcStr, destStr, startTime, apiKey);
     }
 
-    @Override
-    public Object getCostValue(CostArgs arg) {
-        return null;
-    }
+    protected abstract String getCallUrlFormat();
 
-    @Override
-    public boolean isUp() {
-        return true; //TODO
+    protected abstract String getLocationFormat();
+
+    protected abstract String getLocationSeparator();
+
+    private JSONObject runCall(String url) {
+        Request req = new Request.Builder().url(url).build();
+        try {
+            calls++;
+            Response res = client.newCall(req).execute();
+            return new JSONObject(res.body().string());
+        } catch (IOException e) {
+            LoggingUtils.logError(e);
+            return null;
+        }
     }
 
     private static Predicate<TimeDelta> extractComparison(CostArgs args) {
@@ -97,39 +179,39 @@ public class TimeCostProvider implements CostProvider {
         if (compTagArg instanceof Character) {
             switch ((Character) compTagArg) {
                 case '<':
-                    return dt -> dt.getDeltaLong() < border;
+                    return dt -> dt == null || dt.getDeltaLong() < border;
                 case '>':
-                    return dt -> dt.getDeltaLong() > border;
+                    return dt -> dt == null || dt.getDeltaLong() > border;
             }
         }
 
         if (compTagArg instanceof String) {
             switch ((String) compTagArg) {
                 case "<":
-                    return dt -> dt.getDeltaLong() < border;
+                    return dt -> dt == null || dt.getDeltaLong() < border;
                 case "<=":
-                    return dt -> dt.getDeltaLong() <= border;
+                    return dt -> dt == null || dt.getDeltaLong() <= border;
                 case "=<": //Just in case
-                    return dt -> dt.getDeltaLong() <= border;
+                    return dt -> dt == null || dt.getDeltaLong() <= border;
 
                 case ">":
-                    return dt -> dt.getDeltaLong() > border;
+                    return dt -> dt == null || dt.getDeltaLong() > border;
                 case ">=":
-                    return dt -> dt.getDeltaLong() >= border;
+                    return dt -> dt == null || dt.getDeltaLong() >= border;
                 case "=>": //Just in case
-                    return dt -> dt.getDeltaLong() >= border;
+                    return dt -> dt == null || dt.getDeltaLong() >= border;
 
                 case "==":
-                    return dt -> dt.getDeltaLong() == border;
+                    return dt -> dt == null || dt.getDeltaLong() == border;
                 case "===": //For the Javascript devs
-                    return dt -> dt.getDeltaLong() == border;
+                    return dt -> dt == null || dt.getDeltaLong() == border;
                 case "=":   //For the typos
-                    return dt -> dt.getDeltaLong() == border;
+                    return dt -> dt == null || dt.getDeltaLong() == border;
 
                 case "!=":
-                    return dt -> dt.getDeltaLong() != border;
+                    return dt -> dt == null || dt.getDeltaLong() != border;
                 case "!==":
-                    return dt -> dt.getDeltaLong() != border;
+                    return dt -> dt == null || dt.getDeltaLong() != border;
             }
         }
 
