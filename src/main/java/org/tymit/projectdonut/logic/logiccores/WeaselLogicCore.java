1dd8bb3606234f157f81a4a2cb003bd452e15616
ischeinkman <scheinkman.ilan@gmail.com>
2017-05-05 23:14:42 -0700

    Weasel core now bulk requests time costs

--- src/main/java/org/tymit/projectdonut/logic/logiccores/WeaselLogicCore.java
+++ src/main/java/org/tymit/projectdonut/logic/logiccores/WeaselLogicCore.java
@@ -1,17 +1,16 @@
 package org.tymit.projectdonut.logic.logiccores;
 
-import org.tymit.projectdonut.costs.CostArgs;
+import org.tymit.projectdonut.costs.BulkCostArgs;
 import org.tymit.projectdonut.costs.CostCalculator;
-import org.tymit.projectdonut.model.LocationPoint;
+import org.tymit.projectdonut.model.DestinationLocation;
 import org.tymit.projectdonut.model.StartPoint;
-import org.tymit.projectdonut.model.TimeDelta;
 import org.tymit.projectdonut.model.TravelRoute;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 /**
@@ -19,22 +18,23 @@ import java.util.stream.Collectors;
  */
 public class WeaselLogicCore implements LogicCore {
 
-    public static final String TIME_COST_START_TIME_TAG = "starttime";
-    public static final String TIME_COST_COMPARISON_TAG = "comparison";
-    public static final String TIME_COST_COMPARE_VALUE_TAG = "compareto";
-    public static final String TIME_COST_POINT_TWO_TAG = "p2";
     public static final String LAT_TAG = "latitude";
     public static final String LONG_TAG = "longitude";
     public static final String TIME_DELTA_TAG = "timedelta";
     public static final String DEST_LIST_TAG = "DESTS";
+    public static final String START_TIME_TAG = "starttime";
     public static final String ROUTE_LIST_TAG = "ROUTES";
     public static final String TAG = "WEASEL";
-    private static String TIME_COST_TAG = "time";
+    private static final String TIME_COST_START_TIME_TAG = "starttime";
+    private static final String TIME_COST_COMPARISON_TAG = "comparison";
+    private static final String TIME_COST_COMPARE_VALUE_TAG = "compareto";
+    private static final String TIME_COST_POINT_ONE_TAG = "p1";
+    private final static String TIME_COST_TAG = "time";
 
     @Override
     public Map<String, List<Object>> performLogic(Map<String, Object> args) {
 
-        List<LocationPoint> locs = new ArrayList<>();
+        List<StartPoint> locs = new ArrayList<>();
         for (int i = 0; args.containsKey(LAT_TAG + i); i++) {
             double lat = (double) args.get(LAT_TAG + i);
             if (!args.containsKey(LONG_TAG + i)) {
@@ -44,32 +44,35 @@ public class WeaselLogicCore implements LogicCore {
             locs.add(new StartPoint(new double[] { lat, lng }));
         }
 
-        TimeDelta maxDelta = new TimeDelta((Long) args.getOrDefault(TIME_DELTA_TAG, -1));
-
-        Predicate<TravelRoute> timeChecker = rt -> locs.parallelStream()
-                .map(pt -> new CostArgs()
-                        .setCostTag(TIME_COST_TAG)
-                        .setSubject(rt.getDestination())
-                        .setArg(TIME_COST_START_TIME_TAG, rt.getStartTime())
-                        .setArg(TIME_COST_POINT_TWO_TAG, pt)
-                        .setArg(TIME_COST_COMPARE_VALUE_TAG, maxDelta)
-                        .setArg(TIME_COST_COMPARISON_TAG, "<=")
-                )
-                .allMatch(CostCalculator::isWithinCosts);
-
         Map<String, List<Object>> donutResults = LogicCoreHelper.getHelper().runCore("DONUT", args);
 
-        List<Object> dests = new ArrayList<>();
-        List<Object> routes = donutResults.get(ROUTE_LIST_TAG)
-                .parallelStream()
-                .map(rt -> (TravelRoute) rt)
-                .filter(timeChecker)
-                .peek(rt -> dests.add(rt.getDestination()))
-                .collect(Collectors.toList());
+        List<Object> unfilteredRouteObjList = donutResults.get(ROUTE_LIST_TAG);
+
+        Map<DestinationLocation, TravelRoute> routeMap = unfilteredRouteObjList.stream()
+                .map(obj -> (TravelRoute) obj)
+                .collect(ConcurrentHashMap::new, (map, rt) -> map.put(rt.getDestination(), rt), ConcurrentHashMap::putAll);
+
+        Set<BulkCostArgs> ags = locs.stream()
+                .map(pt -> new BulkCostArgs()
+                        .setCostTag(TIME_COST_TAG)
+                        .setArg(TIME_COST_START_TIME_TAG, args.get(START_TIME_TAG))
+                        .setArg(TIME_COST_COMPARE_VALUE_TAG, args.get(TIME_DELTA_TAG))
+                        .setArg(TIME_COST_COMPARISON_TAG, "<=")
+                        .setArg(TIME_COST_POINT_ONE_TAG, pt))
+                .peek(bkarg -> routeMap.keySet().forEach(bkarg::addSubject))
+                .collect(Collectors.toSet());
+
+        for (BulkCostArgs ag : ags) {
+            Map<Object, Boolean> filters = CostCalculator.isWithinCosts(ag);
+            filters.keySet().stream()
+                    .filter(dest -> !filters.getOrDefault(dest, false))
+                    .forEach(routeMap::remove);
+        }
+
 
         Map<String, List<Object>> rval = new ConcurrentHashMap<>();
-        rval.put(ROUTE_LIST_TAG, routes);
-        rval.put(DEST_LIST_TAG, dests);
+        rval.put(ROUTE_LIST_TAG, new ArrayList<>(routeMap.values()));
+        rval.put(DEST_LIST_TAG, new ArrayList<>(routeMap.keySet()));
         return rval;
     }
 
