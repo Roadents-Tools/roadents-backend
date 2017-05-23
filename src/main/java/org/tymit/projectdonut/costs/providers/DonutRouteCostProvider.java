419b7828e231a566456d5a79936388a84bb7dc58
ischeinkman <scheinkman.ilan@gmail.com>
2017-05-23 00:55:12 -0700

    Added routing cost provider based on internal Donut A-B routing logic core

---
+++ src/main/java/org/tymit/projectdonut/costs/providers/DonutRouteCostProvider.java
@@ -0,0 +1,82 @@
+package org.tymit.projectdonut.costs.providers;
+
+import com.google.common.collect.Lists;
+import org.tymit.projectdonut.costs.BulkCostArgs;
+import org.tymit.projectdonut.costs.CostArgs;
+import org.tymit.projectdonut.logic.ApplicationRunner;
+import org.tymit.projectdonut.model.DestinationLocation;
+import org.tymit.projectdonut.model.StartPoint;
+import org.tymit.projectdonut.model.TimePoint;
+import org.tymit.projectdonut.model.TravelRoute;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+/**
+ * Created by ilan on 5/23/17.
+ */
+public class DonutRouteCostProvider extends RouteCostProvider implements BulkCostProvider {
+
+    @Override
+    public boolean isUp() {
+        return true;
+    }
+
+    @Override
+    protected TravelRoute buildRoute(StartPoint a, DestinationLocation b, TimePoint start) {
+        return buildRoutes(a, Lists.newArrayList(b), start).get(0);
+    }
+
+    protected List<TravelRoute> buildRoutes(StartPoint a, List<DestinationLocation> b, TimePoint startTime) {
+
+        Map<String, Object> donutRoutingArgs = new HashMap<>();
+        donutRoutingArgs.put("latitude", a.getCoordinates()[0]);
+        donutRoutingArgs.put("longitude", a.getCoordinates()[1]);
+        donutRoutingArgs.put("starttime", startTime.getUnixTime());
+        for (int i = 0; i < b.size(); i++) {
+            donutRoutingArgs.put("latitude" + i, b.get(i).getCoordinates()[0]);
+            donutRoutingArgs.put("longitude" + i, b.get(i).getCoordinates()[1]);
+        }
+
+        Map<String, List<Object>> callVal = ApplicationRunner.runApplication("DONUTAB", donutRoutingArgs);
+        return callVal.get("ROUTES").stream().map(rt -> (TravelRoute) rt).collect(Collectors.toList());
+    }
+
+    @Override
+    public Map<Object, Boolean> isWithinCosts(BulkCostArgs args) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Map<Object, Object> getCostValue(BulkCostArgs args) {
+
+        Collection<CostArgs> unorderedArgs = args.splitSingular().values();
+        List<Object> keys = new ArrayList<>();
+        List<DestinationLocation> dests = new ArrayList<>();
+        CostArgs randomSingleArgs = null;
+
+        for (CostArgs singArg : unorderedArgs) {
+            if (randomSingleArgs == null) randomSingleArgs = singArg;
+            keys.add(singArg.getSubject());
+            dests.add(extractEnd(singArg));
+        }
+
+        int destSize = dests.size();
+        StartPoint start = extractStart(randomSingleArgs);
+        TimePoint startTime = new TimePoint(extractStartTime(randomSingleArgs), "America/Los Angeles");
+
+        List<TravelRoute> routes = buildRoutes(start, dests, startTime);
+
+        Map<Object, Object> rval = new ConcurrentHashMap<>();
+        for (int i = 0; i < destSize; i++) {
+            rval.put(keys.get(i), routes.get(i));
+        }
+        return rval;
+    }
+}
