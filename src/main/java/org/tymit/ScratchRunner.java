c54bc2468ba5981f7fb0340e712c94195cd921e7
ischeinkman <scheinkman.ilan@gmail.com>
2017-07-11 21:34:15 -0700

    Added scratch helper method for bulk loading zips into a postgres db

--- src/main/java/org/tymit/ScratchRunner.java
+++ src/main/java/org/tymit/ScratchRunner.java
@@ -1,5 +1,14 @@
 package org.tymit;
 
+import org.tymit.projectdonut.model.location.TransStation;
+import org.tymit.projectdonut.stations.gtfs.GtfsProvider;
+import org.tymit.projectdonut.stations.postgresql.PostgresqlStationDbCache;
+import org.tymit.projectdonut.utils.LoggingUtils;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by ilan on 1/5/17.
  */
@@ -8,8 +17,25 @@ public class ScratchRunner {
 
     public static void main(String[] args) throws Exception {
 
+        LoggingUtils.setPrintImmediate(true);
+
     }
 
+
+    private static boolean loadtransitzips(String rootdirectory, String dburl) {
+        PostgresqlStationDbCache db = new PostgresqlStationDbCache(dburl);
+        File rootFile = new File(rootdirectory);
+        return Arrays.stream(rootFile.listFiles())
+                .parallel()
+                .map(GtfsProvider::new)
+                .map(GtfsProvider::getUpdatedStations)
+                .allMatch(col -> {
+                    for (List<TransStation> stats : col.values()) {
+                        if (!db.putStations(stats)) return false;
+                    }
+                    return true;
+                });
+    }
 
 }
 
