7d7c5d2c9c3bf3728dab609b4a8fee61ea1fba52
ischeinkman <scheinkman.ilan@gmail.com>
2017-07-14 17:24:31 -0700

    Removed strange LocationProviderHelper type map logic.

--- src/main/java/org/tymit/projectdonut/locations/helpers/LocationProviderHelper.java
+++ src/main/java/org/tymit/projectdonut/locations/helpers/LocationProviderHelper.java
@@ -6,13 +6,9 @@ import org.tymit.projectdonut.locations.test.TestLocationProvider;
 import org.tymit.projectdonut.model.location.DestinationLocation;
 import org.tymit.projectdonut.model.location.LocationType;
 
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by ilan on 7/7/16.
@@ -21,12 +17,10 @@ public class LocationProviderHelper {
 
     private static boolean isTest = false;
     private static LocationProviderHelper instance = new LocationProviderHelper();
-    private final ConcurrentMap<LocationType, Set<LocationProvider>> typeToProviders;
     private LocationProvider[] allProviders;
 
     private LocationProviderHelper() {
         initializeProvidersList();
-        typeToProviders = new ConcurrentHashMap<>();
     }
 
     //We use a method in cases with a lot of boilerplate
@@ -50,25 +44,15 @@ public class LocationProviderHelper {
     }
 
     public List<DestinationLocation> getLocations(double[] center, double range, LocationType type) {
-        if (typeToProviders.containsKey(type) && typeToProviders.get(type).size() > 0) {
-            return typeToProviders.get(type).stream()
-                    .filter(LocationProvider::isUsable)
-                    .map(provider -> provider.queryLocations(center, range, type))
-                    .filter(Objects::nonNull)
-                    .findFirst()
-                    .orElse(Collections.emptyList());
-        }
-        for (LocationProvider attemptProvider : allProviders) {
-            if (!attemptProvider.isValidType(type)) continue;
 
-            typeToProviders.putIfAbsent(type, Collections.synchronizedSet(new HashSet<>()));
-
-            typeToProviders.get(type).add(attemptProvider);
-            if (!attemptProvider.isUsable()) continue;
-            List<DestinationLocation> allPoints = attemptProvider.queryLocations(center, range, type);
-            if (allPoints != null) return allPoints;
-        }
-        return null;
+        return Arrays.stream(allProviders)
+                .filter(attemptProvider -> attemptProvider.isValidType(type))
+                .filter(LocationProvider::isUsable)
+                .map(attemptProvider -> attemptProvider.queryLocations(center, range, type))
+                .filter(Objects::nonNull)
+                .filter(a -> !a.isEmpty())
+                .findFirst()
+                .orElse(null);
     }
 
     public void closeAllProviders() {
