2ac577a6eb37f61c5956ddb2ae064d97f3a77213
ischeinkman <scheinkman.ilan@gmail.com>
2017-07-18 19:18:22 -0700

    Added tests for reflexivity on LocationUtils.

--- src/test/java/org/tymit/projectdonut/utils/LocationUtilsTest.java
+++ src/test/java/org/tymit/projectdonut/utils/LocationUtilsTest.java
@@ -9,17 +9,23 @@ import org.junit.Test;
 public class LocationUtilsTest {
 
     @Test
-    public void walkTimeDistanceConversions() throws Exception {
-        long testTime = 1000L * 60 * 60; //1 hour
-
-        double calcedDistanceMiles = LocationUtils.timeToWalkDistance(testTime, true);
-        double calcedDistanceKm = LocationUtils.timeToWalkDistance(testTime, false);
-        Assert.assertEquals(1.609, calcedDistanceKm / calcedDistanceMiles, 0.0001);
-
-        long convertedBackMiles = LocationUtils.distanceToWalkTime(calcedDistanceMiles, true);
-        long convertedBackKm = LocationUtils.distanceToWalkTime(calcedDistanceKm, false);
-        Assert.assertEquals(testTime, convertedBackKm);
-        Assert.assertEquals(testTime, convertedBackMiles);
+    public void timeBetween() throws Exception {
+        double[] p1 = { 43, -71 };
+        double[] p2 = { 42, -70 };
+        double distance = LocationUtils.distanceBetween(p1, p2, true);
+        long timeActual = LocationUtils.timeBetween(p1, p2);
+        assertEqualsDelta(distance, LocationUtils.timeToWalkDistance(timeActual, true), 1. / 5280);
     }
 
+    private static void assertEqualsDelta(double value1, double value2, double delta) {
+        Assert.assertTrue(Math.abs(value1 - value2) <= delta);
+    }
+
+    @Test
+    public void milesToMeters() throws Exception {
+        double actual = LocationUtils.milesToMeters(1);
+        double expected = 1609.34;
+        assertEqualsDelta(actual, expected, 1);
+        assertEqualsDelta(1, LocationUtils.metersToMiles(actual), 1. / 5280);
+    }
 }
