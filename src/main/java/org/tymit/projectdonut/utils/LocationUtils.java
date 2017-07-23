f115edd389323510d6f63591feb056031ceb1db5
ischeinkman <scheinkman.ilan@gmail.com>
2017-07-22 21:59:01 -0700

    Created methods in LocationUtils using Distance and depreciated the primitives methods.

--- src/main/java/org/tymit/projectdonut/utils/LocationUtils.java
+++ src/main/java/org/tymit/projectdonut/utils/LocationUtils.java
@@ -1,5 +1,7 @@
 package org.tymit.projectdonut.utils;
 
+import org.tymit.projectdonut.model.distance.Distance;
+import org.tymit.projectdonut.model.distance.DistanceUnits;
 import org.tymit.projectdonut.model.location.LocationPoint;
 import org.tymit.projectdonut.model.time.TimeDelta;
 
@@ -17,6 +19,7 @@ public class LocationUtils {
     private static final double AVG_WALKING_SPEED_KPH = 5.0;
     private static final double SAFETY_FACTOR = 1;
 
+    @Deprecated
     public static long timeBetween(double[] l1, double[] l2) {
         return distanceToWalkTime(distanceBetween(l1, l2, true), true);
     }
@@ -25,6 +28,7 @@ public class LocationUtils {
         return new TimeDelta(timeBetween(a.getCoordinates(), b.getCoordinates()));
     }
 
+    @Deprecated
     public static double distanceBetween(double[] l1, double[] l2, boolean miles) {
 
         double dLat = Math.toRadians(l2[0] - l1[0]);
@@ -37,25 +41,37 @@ public class LocationUtils {
         return c * ((miles) ? EARTH_RADIUS_MI : EARTH_RADIUS_KM);
     }
 
-    public static double distanceBetween(LocationPoint a, LocationPoint b, boolean miles) {
-        return distanceBetween(a.getCoordinates(), b.getCoordinates(), miles);
-    }
-
+    @Deprecated
     public static long distanceToWalkTime(double distance, boolean miles) {
         double hours = (miles) ? distance / AVG_WALKING_SPEED_MPH : distance / AVG_WALKING_SPEED_KPH;
         double millis = hours * 1000.0 * 60.0 * 60.0 * SAFETY_FACTOR;
         return (long) millis;
     }
 
+    public static Distance distanceBetween(LocationPoint a, LocationPoint b) {
+        return new Distance(distanceBetween(a.getCoordinates(), b.getCoordinates(), false), DistanceUnits.KILOMETERS);
+    }
+
+    public static TimeDelta distanceToWalkTime(Distance distance) {
+        return new TimeDelta(distanceToWalkTime(distance.inKilometers(), false));
+    }
+
+    public static Distance timeToWalkDistance(TimeDelta time) {
+        return new Distance(timeToWalkDistance(time.getDeltaLong(), false), DistanceUnits.KILOMETERS);
+    }
+
+    @Deprecated
     public static double timeToWalkDistance(long time, boolean miles) {
         double timeHours = time / 1000.0 / 60.0 / 60.0;
         return (miles) ? AVG_WALKING_SPEED_MPH * timeHours / SAFETY_FACTOR : AVG_WALKING_SPEED_KPH * timeHours / SAFETY_FACTOR;
     }
 
+    @Deprecated
     public static double milesToMeters(double miles){
         return miles * 1000 * AVG_WALKING_SPEED_KPH/AVG_WALKING_SPEED_MPH;
     }
 
+    @Deprecated
     public static double metersToMiles(double meters) {
         return meters / 1000 * AVG_WALKING_SPEED_MPH / AVG_WALKING_SPEED_KPH;
     }
