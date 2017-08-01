ab5809379fae891beb3c50eed31ddcf515e62227
ischeinkman <scheinkman.ilan@gmail.com>
2017-07-31 22:43:23 -0700

    ScratchRunner now has options for map generation.

--- src/main/java/org/tymit/ScratchRunner.java
+++ src/main/java/org/tymit/ScratchRunner.java
@@ -1,7 +1,10 @@
 package org.tymit;
 
 import org.tymit.displayers.lambdacontroller.LambdaHandler;
+import org.tymit.displayers.testdisplay.mapsareadrawer.MapsPageGenerator;
 import org.tymit.projectdonut.model.location.TransStation;
+import org.tymit.projectdonut.model.time.TimeDelta;
+import org.tymit.projectdonut.model.time.TimePoint;
 import org.tymit.projectdonut.stations.gtfs.GtfsProvider;
 import org.tymit.projectdonut.stations.postgresql.PostgresqlStationDbCache;
 import org.tymit.projectdonut.utils.LoggingUtils;
@@ -15,6 +18,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by ilan on 1/5/17.
@@ -29,11 +33,14 @@ public class ScratchRunner {
         List<String> data = null;
         String outputFile = null;
         for (int i = 0; i < args.length; i++) {
+            if ("--map".equals(args[i])) {
+                mapLocations(args);
+                return;
+            }
             if ("-f".equals(args[i]) && args.length > i + 1) {
                 String filePath = args[i + 1];
                 data = new ArrayList<>(Files.readAllLines(Paths.get(filePath)));
-            }
-            if ("-o".equals(args[i]) && args.length > i + 1) {
+            } else if ("-o".equals(args[i]) && args.length > i + 1) {
                 outputFile = args[i + 1];
             }
         }
@@ -71,6 +78,36 @@ public class ScratchRunner {
                     }
                     return true;
                 });
+    }
+
+    private static void mapLocations(String[] args) {
+        TimePoint startTime = new TimePoint(1500829200 * 1000L, "America/New_York");
+        TimeDelta maxDelta = new TimeDelta(1800 * 1000);
+        String outputDir = "~";
+        String filePath = null;
+
+        for (int i = 0; i < args.length; i++) {
+            if ("-f".equals(args[i]) && args.length > i + 1) {
+                filePath = args[i + 1];
+            } else if ("-o".equals(args[i]) && args.length > i + 1) {
+                outputDir = args[i + 1];
+            }
+        }
+
+        if (filePath == null) LoggingUtils.logError("ScratchRunner", "Need an input to run.");
+        mapLocations(filePath, startTime, maxDelta, outputDir);
+    }
+
+    private static void mapLocations(String file, TimePoint startTime, TimeDelta maxDelta, String outputDir) {
+        AtomicLong count = new AtomicLong(0);
+        MapsPageGenerator.generateIndividualPagesFromFile(file, startTime, maxDelta)
+                .forEach((LoggingUtils.WrappedConsumer<String>) pg -> {
+                    String filename = "mapnum" + count.getAndIncrement() + ".html";
+                    String path = outputDir + "/" + filename;
+                    Files.createFile(Paths.get(path));
+                    Files.write(Paths.get(path), pg.getBytes());
+                });
+
     }
 
 }
