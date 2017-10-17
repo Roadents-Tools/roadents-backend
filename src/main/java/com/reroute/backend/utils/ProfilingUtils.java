a753a14552a398d3a83b3424311ec3d7fdcb144e
ischeinkman <scheinkman.ilan@gmail.com>
2017-10-17 14:25:24 -0700

    Profiler Improvements

--- src/main/java/com/reroute/backend/utils/ProfilingUtils.java
+++ src/main/java/com/reroute/backend/utils/ProfilingUtils.java
@@ -2,7 +2,10 @@ package com.reroute.backend.utils;
 
 import com.google.common.collect.Sets;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,7 +25,7 @@ public class ProfilingUtils {
         }
         profilerThread = new Thread(() -> {
             while (profilerThread != null) {
-                LoggingUtils.logMessage("Profilter", printData());
+                LoggingUtils.logMessage("Profiler", "Profiling statistics:\n" + printData());
                 try {
                     Thread.sleep(millis);
                 } catch (InterruptedException e) {
@@ -34,20 +37,44 @@ public class ProfilingUtils {
     }
 
     public static String printData() {
+        ProfilingUtils.MethodTimer ourtm = ProfilingUtils.startTimer("Profile");
         StringBuilder builder = new StringBuilder();
 
-        for (String methodTag : getMethodTags()) {
-            String formatted = String.format("Method: %s    Time: %d    Percent: %f\n", methodTag, getTotalTime(methodTag), getTimeAsPercent(methodTag));
-            builder.append(formatted);
+        List<String> toSort = new ArrayList<>();
+        toSort.addAll(getMethodTags());
+        toSort.sort(Comparator.comparing(ProfilingUtils::getTotalTime).reversed());
+        for (String methodTag : toSort) {
+            String format = String.format("Method: %45s    Time: %10d    Percent: %f\n", methodTag, getTotalTime(methodTag), getTimeAsPercent(methodTag));
+            builder.append(format);
         }
-        long timeTotal = times.keySet().parallelStream().mapToLong(ProfilingUtils::getTotalTime).sum();
-        builder.append("Total Time: ").append(timeTotal).append(" Total Percent: 100\n");
+        long timeTotal = times.values().parallelStream()
+                .flatMap(Collection::parallelStream)
+                .mapToLong(MethodTimer::getTimeDelta)
+                .sum();
+        builder.append("Sum Time: ").append(timeTotal).append(" Sum Percent: 100\n");
+        long startTime = times.values()
+                .stream()
+                .flatMap(Collection::stream)
+                .mapToLong(tm -> tm.startTime)
+                .min()
+                .orElse(-1);
+        if (startTime > 0) {
+            long now = System.currentTimeMillis();
+            long diff = now - startTime;
+            double percent = timeTotal * 100. / diff;
+            builder.append("Total Time: ").append(diff).append(" Total Percent: ").append(percent);
+        } else {
+            builder.append("Total Time: 0 Total Percent: undefined");
+        }
+
+
 
         times.values().stream()
                 .flatMap(Collection::stream)
                 .filter(timer -> timer.endTime == -1)
                 .forEach(timer -> LoggingUtils.logMessage("Profiler", timer.toString() + "\n"));
 
+        ourtm.stop();
         return builder.toString();
     }
 
