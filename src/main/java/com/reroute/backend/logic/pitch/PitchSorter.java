364489b5fc752b57d089092358ea6587f8cac4ca
ischeinkman <scheinkman.ilan@gmail.com>
2017-10-20 20:05:00 -0700

    Fixed node MIN and MAX being switched.

--- src/main/java/com/reroute/backend/logic/pitch/PitchSorter.java
+++ src/main/java/com/reroute/backend/logic/pitch/PitchSorter.java
@@ -8,14 +8,14 @@ import java.util.Comparator;
 public enum PitchSorter {
     MIN_TIME("time MIN", Comparator.comparing(t -> t.getTime().getDeltaLong())),
     MAX_TIME("time MAX", Comparator.comparing((t -> t.getTime().getDeltaLong()), Comparator.reverseOrder())),
-    MIN_NODE("node MIN", Comparator.comparing(t -> t.getRoute().size(), Comparator.reverseOrder())),
-    MAX_NODE("node MAX", Comparator.comparing(t -> t.getRoute().size())),
+    MAX_NODE("node MIN", Comparator.comparing(t -> t.getRoute().size())),
+    MIN_NODE("node MAX", Comparator.comparing(t -> t.getRoute().size(), Comparator.reverseOrder())),
     MIN_DIST("dist MIN", Comparator.comparing(t -> LocationUtils.distanceBetween(t.getStart(), t.getCurrentEnd())
             .inMeters())),
     MAX_DIST("dist MAX", Comparator.comparing(t -> LocationUtils.distanceBetween(t.getStart(), t.getCurrentEnd())
             .inMeters(), Comparator.reverseOrder())),
-    MIN_LENGTH("leng MIN", Comparator.comparing(t -> t.getDisp().inMeters())),
-    MAX_LENGTH("leng MAX", Comparator.comparing(t -> t.getDisp().inMeters(), Comparator.reverseOrder())),
+    MIN_LENGTH("disp MIN", Comparator.comparing(t -> t.getDisp().inMeters())),
+    MAX_LENGTH("disp MAX", Comparator.comparing(t -> t.getDisp().inMeters(), Comparator.reverseOrder())),
     MIN_LABOR("labor MIN", Comparator.comparing(t -> t.getWalkDisp().inMeters())),
     MAX_LABOR("labor MAX", Comparator.comparing(t -> t.getWalkDisp().inMeters(), Comparator.reverseOrder())),;
 
