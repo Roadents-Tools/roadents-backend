5b5743652f0846b8070b45a59ccde05855eab89b
ischeinkman <scheinkman.ilan@gmail.com>
2017-12-18 23:44:11 -0800

    Added terminal command to load a zip file from a URL to our new Postgresql schema; cleaned up ScratchRunner.

--- src/main/scala/com/reroute/ScratchRunner.java
+++ src/main/scala/com/reroute/ScratchRunner.java
@@ -3,26 +3,14 @@ package com.reroute;
 import com.reroute.backend.model.distance.Distance;
 import com.reroute.backend.model.distance.DistanceUnits;
 import com.reroute.backend.model.location.StartPoint;
-import com.reroute.backend.model.location.TransChain;
-import com.reroute.backend.model.location.TransStation;
-import com.reroute.backend.model.time.TimeDelta;
-import com.reroute.backend.model.time.TimePoint;
-import com.reroute.backend.stations.WorldInfo;
-import com.reroute.backend.stations.gtfs.GtfsProvider;
-import com.reroute.backend.stations.postgresql.PostgresqlDonutDb;
-import com.reroute.backend.stations.redis.RedisDonutCache;
+import com.reroute.backend.stations.gtfs.GtfsPostgresLoader;
 import com.reroute.backend.stations.transitland.TransitlandApiDb;
 import com.reroute.backend.utils.LoggingUtils;
 import com.reroute.displayers.restcontroller.SparkHandler;
+import scala.util.Try;
 
-import java.io.File;
 import java.net.URL;
-import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,10 +24,6 @@ public class ScratchRunner {
         LoggingUtils.setPrintImmediate(true);
         try {
             for (String arg : args) {
-                if ("--cache".equals(arg)) {
-                    cache(args);
-                    return;
-                }
                 if ("--spark".equals(arg)) {
                     runSpark(args);
                     return;
@@ -48,11 +32,12 @@ public class ScratchRunner {
                     listUrls(args);
                     return;
                 }
-                if ("--load".equals(arg)) {
-                    loadtransitzips(args);
+                if ("--loadNew".equals(arg)) {
+                    loadZipsNew(args);
                     return;
                 }
             }
+            System.out.println("ARGUMENT NOT FOUND, EXITING...");
         } catch (Exception e) {
             LoggingUtils.logError(e);
         }
@@ -87,72 +72,40 @@ public class ScratchRunner {
         TransitlandApiDb apidb = new TransitlandApiDb();
         Map<String, String> skipBad = new HashMap<>();
         skipBad.put("license_use_without_attribution", "no");
-        apidb.getFeedsInArea(center, range, null, skipBad).stream()
-                .peek(System.out::println)
-                .forEach(url -> {
-                    String result;
-
-                    String rval = "/home/main/Downloads/tzip/" + url.getFile().replaceAll("/", "__");
-
-                    File zipFile = new File(rval);
-                    try {
-                        zipFile.delete();
-                        zipFile.createNewFile();
-                        zipFile.setWritable(true);
-                        URLConnection con = url.openConnection();
-                        URL trurl = con.getHeaderField("Location") == null
-                                ? url
-                                : new URL(con.getHeaderField("Location"));
-                        Files.copy(trurl.openStream(), zipFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                        result = rval;
-                    } catch (Exception e) {
-                        LoggingUtils.logError(e);
-                        result = null;
-                    }
-                });
+        apidb.getFeedsInArea(center, range, null, skipBad).forEach(System.out::println);
     }
 
-    private static void loadtransitzips(String[] args) {
-        String dburl = PostgresqlDonutDb.DB_URLS[1];
-        String rootdir = null;
+    private static void loadZipsNew(String[] args) {
+        String zipurl = null;
+        String db = null;
         for (int i = 0; i < args.length; i++) {
-            if ("-d".equals(args[i]) && args.length > i + 1) {
-                rootdir = args[i + 1];
-            }
-            if ("-url".equals(args[i]) && args.length > i + 1) {
-                dburl = args[i + 1];
+            if ("--zip".equals(args[i])) {
+                zipurl = args[i + 1];
+            } else if ("--db".equals(args[i])) {
+                db = args[i + 1];
             }
         }
 
-        if (dburl == null || rootdir == null) {
-            LoggingUtils.logError("ScratchRunner", "Got null arguments for database populator. Returning.");
+        if (zipurl == null || db == null) {
+            System.out.printf("Got null args:\ndb = %s\nzip = %s\n", db, zipurl);
             return;
         }
 
-        PostgresqlDonutDb db = new PostgresqlDonutDb(dburl);
-        File rootFile = new File(rootdir);
-        Arrays.stream(rootFile.listFiles()).parallel().allMatch(file -> {
-            LoggingUtils.logMessage("DB Loader", "Starting URL %s.", file.getName());
-            Map<TransChain, List<TransStation>> mp = new GtfsProvider(file).getUpdatedStations();
-            if (!mp.values().stream().allMatch(db::putStations)) {
-                LoggingUtils.logError("DB Loader", "ERR on URL %s.", file.getName());
-                return false;
-            }
-            LoggingUtils.logMessage("DB Loader", "Finished URL %s.", file.getName());
-            return true;
-        });
-    }
-
-    private static void cache(String[] args) {
-        RedisDonutCache cache = new RedisDonutCache("debstop.dynamic.ucsd.edu", 6379);
-        PostgresqlDonutDb db = new PostgresqlDonutDb(PostgresqlDonutDb.DB_URLS[0]);
-        WorldInfo req = new WorldInfo.Builder()
-                .setCenter(new StartPoint(40.676843, -74.041512))
-                .setRange(new Distance(1000, DistanceUnits.KILOMETERS))
-                .setStartTime(TimePoint.NULL)
-                .setMaxDelta(new TimeDelta(Long.MAX_VALUE))
-                .build();
-        cache.putWorld(req, db.getWorld(req.getCenter(), req.getRange(), req.getStartTime(), req.getMaxDelta()));
+        URL url;
+        try {
+            url = new URL(zipurl);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("BAD URL: " + zipurl);
+            return;
+        }
+        GtfsPostgresLoader loader = new GtfsPostgresLoader(url);
+        Try<Object> res = loader.load(db, "donut", "donutpass");
+        if (res.isFailure()) {
+            System.out.printf("Got args:\ndb = %s\nzip = %s\n", db, zipurl);
+            res.toEither().left().get().printStackTrace();
+        }
+        System.out.println("Build complete.");
     }
 
 }
---
+++ src/main/scala/com/reroute/backend/stations/gtfs/GtfsPostgresLoader.scala
@@ -0,0 +1,79 @@
+package com.reroute.backend.stations.gtfs
+
+import java.io.File
+import java.net.URL
+import java.nio.file.{Files, StandardCopyOption}
+import java.sql.{DriverManager, Statement}
+import java.util.Properties
+
+import org.onebusaway.gtfs.GtfsDatabaseLoaderMain
+
+import scala.util.Try
+
+class GtfsPostgresLoader(val url: URL) {
+
+  val source: String = url.toString
+  val zipFileOpt = Try {
+    val zipFile = new File(url.getFile.replaceAll("/", "__"))
+    zipFile.delete
+    zipFile.createNewFile
+    zipFile.setWritable(true)
+    System.setProperty("jsse.enableSNIExtension", "false")
+    System.setProperty("sun.security.ssl.allowUnsafeRenegotiation", "true")
+    val basecon = url.openConnection()
+    println("OPENED BASE!")
+    val trurl = if (basecon.getHeaderField("Location") == null) url else new URL(basecon.getHeaderField("Location"))
+    println("GOT TRUE!")
+    val con = if (trurl == url) basecon else trurl.openConnection()
+    val inp = con.getInputStream
+    println("INPD")
+    Files.copy(inp, zipFile.toPath, StandardCopyOption.REPLACE_EXISTING)
+    println("COPIED!")
+    zipFile
+  }
+
+  def load(db: String, userName: String = "donut", userPass: String = "donutpass"): Try[Boolean] = {
+
+    val dbconOpt = Try {
+      Class.forName("org.postgresql.Driver")
+      val props = new Properties()
+      props.setProperty("user", userName)
+      props.setProperty("password", userPass)
+      props.setProperty("ssl", "true")
+      props.setProperty("sslfactory", "org.postgresql.ssl.NonValidatingFactory")
+      props.setProperty("sslcompression", "true")
+      DriverManager.getConnection(db, props)
+    }
+
+    val res = for (zip <- zipFileOpt; con <- dbconOpt) yield {
+      val args = Array(
+        s"--username=$userName",
+        s"--password=$userPass",
+        s"--url=$db",
+        "--driverClass=org.postgresql.Driver",
+        zip.getAbsolutePath
+      )
+      GtfsDatabaseLoaderMain.main(args)
+
+      val feedStatement = con.createStatement()
+      feedStatement.execute(
+        s"""INSERT INTO metadata(feed_static_url, update_time) VALUES ('$source', ${System.currentTimeMillis() / 1000})""",
+        Statement.RETURN_GENERATED_KEYS)
+      val rs = feedStatement.getGeneratedKeys
+      rs.next()
+      val dataId = rs.getInt("id")
+      feedStatement.close()
+
+      val updateStatements = con.createStatement()
+      updateStatements.addBatch(s"UPDATE gtfs_agencies SET metadata_id=$dataId WHERE metadata_id=NULL")
+      updateStatements.addBatch(s"UPDATE gtfs_feed_info SET metadata_id=$dataId WHERE metadata_id=NULL")
+      updateStatements.addBatch(s"UPDATE gtfs_stops SET latlng=ST_POINT(lon, lat)::geography")
+      updateStatements.executeBatch()
+      updateStatements.close()
+      true
+    }
+    dbconOpt.foreach(_.close())
+    res
+  }
+
+}
