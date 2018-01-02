e1477a66cd08d134f223baeb281f489b7c09cf30
ischeinkman <scheinkman.ilan@gmail.com>
2018-01-01 16:16:37 -0800

    Generified SparkHandler to be able to easily map all logic cores.

--- src/main/scala/com/reroute/displayers/restcontroller/SparkHandler.scala
+++ src/main/scala/com/reroute/displayers/restcontroller/SparkHandler.scala
@@ -1,7 +1,8 @@
 package com.reroute.displayers.restcontroller
 
-import com.reroute.backend.logic.ApplicationResult
 import com.reroute.backend.logic.donut.{DonutCore, DonutRequest}
+import com.reroute.backend.logic.interfaces.LogicCore
+import com.reroute.backend.logic.{ApplicationRequest, ApplicationResult, RequestMapper}
 import com.reroute.backend.model.json.RouteJsonOutputer
 import spark.{Request, Response, Spark}
 
@@ -9,23 +10,27 @@ import scala.collection.JavaConverters._
 
 object SparkHandler {
 
+  val paths: Seq[SparkArg[_ <: ApplicationRequest]] = Seq(
+    SparkArg("/generator", DonutCore, DonutRequest)
+  )
+
   def main(args: Array[String]): Unit = {
-    Spark.get("/generator", runDonut)
+    paths.foreach(arg => Spark.get(arg.path, runPath(arg, _, _)))
     Spark.before((_, response) => {
       response.header("Access-Control-Allow-Origin", "*")
     })
   }
 
-  def runDonut(req: Request, res: Response): String = {
+  def runPath[T <: ApplicationRequest](arg: SparkArg[T], req: Request, res: Response): String = {
     res.`type`("Application/JSON")
     val params = req.queryMap.toMap.asScala.map(params => params._1 -> params._2(0)).toMap
-    val algoReq = DonutRequest.buildQuery(params) match {
-      case Right(reqa) => reqa
+    val logicReq = arg.parser.buildQuery(params) match {
+      case Right(good) => good
       case Left(err) =>
         res.status(400)
         return s"""{ "error" : 400, "message" : "${err.replace("\"", "\\\"")}" }"""
     }
-    DonutCore.runLogic(algoReq) match {
+    arg.core.runLogic(logicReq) match {
       case ApplicationResult.Result(items) =>
         res.status(200)
         items.map(RouteJsonOutputer.output).mkString("[", ", ", "]")
@@ -35,3 +40,5 @@ object SparkHandler {
     }
   }
 }
+
+case class SparkArg[T <: ApplicationRequest](path: String, core: LogicCore[T], parser: RequestMapper[T])
