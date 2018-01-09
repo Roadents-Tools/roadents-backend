5d37250a4fac3d9d56b4a46c3598d630d6fb0a58
ischeinkman <scheinkman.ilan@gmail.com>
2018-01-09 13:22:37 -0800

    Added database based on OSM JSON loaded into Postgresql.

--- src/main/scala/com/reroute/backend/locations/helpers/LocationProviderManager.scala
+++ src/main/scala/com/reroute/backend/locations/helpers/LocationProviderManager.scala
@@ -2,8 +2,10 @@ package com.reroute.backend.locations.helpers
 
 import com.reroute.backend.locations.LocationsRequest
 import com.reroute.backend.locations.interfaces.LocationProvider
+import com.reroute.backend.locations.osmpostgres.PostgresModifiedOsmDb
 import com.reroute.backend.locations.test.TestLocationProvider
 import com.reroute.backend.model.location.ReturnedLocation
+import com.reroute.backend.utils.postgres.PostgresConfig
 
 import scala.collection.mutable
 
@@ -12,12 +14,16 @@ object LocationProviderManager {
   private var isTest = false
   private var loadedProviders: Seq[LocationProvider] = Nil
 
+  private val psqlConfigs = Seq(
+    PostgresConfig(
+      dbname = "localdb",
+      dburl = "jdbc:postgresql://debstop.dynamic.ucsd.edu:5433/locations"
+    )
+  )
+
   private def initializeProvidersList(): Unit = {
-    if (isTest) {
-      loadedProviders = Seq()
-    }
-    else {
-      loadedProviders = Seq(new TestLocationProvider())
+    loadedProviders = if (isTest) Seq(new TestLocationProvider()) else {
+      psqlConfigs.map(new PostgresModifiedOsmDb(_))
     }
   }
 
@@ -32,7 +38,7 @@ object LocationProviderManager {
     var rval = mutable.Map[LocationsRequest, Seq[ReturnedLocation]]()
     providers
       .filter(_.isUp)
-      .takeWhile(_ => request.exists(!rval.contains(_)))
+      .takeWhile(_ => request.exists(req => !rval.contains(req)))
       .map(prov => prov.queryLocations(request.filter(prov.validityFilter)))
       .foreach(rval.++=)
     rval.toMap
